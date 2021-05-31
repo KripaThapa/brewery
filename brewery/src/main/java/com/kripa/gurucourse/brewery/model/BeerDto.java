9e1b877f4ccd6502e0aa0a79c240a3ba52c8b920
KripaThapa <creationinmind@gmail.com>
2021-05-31 14:36:14 -0500

    modified

--- brewery/src/main/java/com/kripa/gurucourse/brewery/model/BeerDto.java
+++ brewery/src/main/java/com/kripa/gurucourse/brewery/model/BeerDto.java
@@ -1,44 +1,17 @@
 package com.kripa.gurucourse.brewery.model;
 
+import lombok.*;
 
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class BeerDto {
 
     private Long id;
     private String beerName;
     private String beerStyle;
     private Long upc;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBeerName() {
-        return beerName;
-    }
-
-    public void setBeerName(String beerName) {
-        this.beerName = beerName;
-    }
-
-    public String getBeerStyle() {
-        return beerStyle;
-    }
-
-    public void setBeerStyle(String beerStyle) {
-        this.beerStyle = beerStyle;
-    }
-
-    public Long getUpc() {
-        return upc;
-    }
-
-    public void setUpc(Long upc) {
-        this.upc = upc;
-    }
-
 
 }
--- brewery/src/main/java/com/kripa/gurucourse/brewery/model/CustomerDto.java
+++ brewery/src/main/java/com/kripa/gurucourse/brewery/model/CustomerDto.java
@@ -1,32 +1,18 @@
 package com.kripa.gurucourse.brewery.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CustomerDto {
 
     private Long customerId;
     private String customerName;
 
-    public CustomerDto(){}
-
-    public CustomerDto(Long customerId, String customerName) {
-        this.customerId = customerId;
-        this.customerName = customerName;
-    }
-
-    public Long getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(Long customerId) {
-        this.customerId = customerId;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-
 }
+
